b1d0df6bb9d7afcf6834350e6f9502c8277a72ce
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-06-06 16:15:08 +0600

    Update UX

--- src/components/quziDone.js
+++ src/components/quziDone.js
@@ -1,9 +1,16 @@
-const QuizDone = (props)=> {
-    return (
-        <div className="w-full min-h-[300px] flex items-center justify-center flex-col gap-4">
-            <h3 className="text-3xl text-black max-w-[80%] text-center">You have {props.point} correct answer</h3>
-            <button onClick={()=> props.backToHome()} className="px-4 py-3 bg-main text-white opacity-80 hover:opacity-100 transition-all">Go Back To Welcome Page</button>
-        </div>
-    )
-}
-export default QuizDone
+const QuizDone = (props) => {
+  return (
+    <div className="w-full min-h-[300px] flex items-center justify-center flex-col gap-4">
+      <h3 className="text-3xl text-black max-w-[80%] text-center">
+        You have {props.point} correct answer
+      </h3>
+      <button
+        onClick={() => props.backToHome()}
+        className="px-4 py-3 bg-main text-white opacity-80 hover:opacity-100 transition-all rounded-md"
+      >
+        Go Back To Welcome Page
+      </button>
+    </div>
+  );
+};
+export default QuizDone;
